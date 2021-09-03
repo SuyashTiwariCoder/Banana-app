68540b17ba0bbc70735ef00b6663cc68aaffc7dc
SuyashTiwariCoder <89460164+SuyashTiwariCoder@users.noreply.github.com>
2021-09-03 16:25:05 +0530

    except styling everything done

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click" , clickHandler)
+btnTranslate.addEventListener("click", clickHandler);
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+function clickHandler() {
+  var inputText = txtInput.value;
 
-function clickHandler()
-{
-    console.log("clicked");
-    console.log("input --> " , txtInput.value)
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      outputDiv.innerHTML = json.contents.translated;
+    });
+}
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
 }
